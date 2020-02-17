67bb2e1a5d01a424f719c58478201dc94598e740
Syed Asad Ullah Taimoor <syed.asad@msn.com>
2020-02-17 19:11:50 +0500

    Add project files.

---
+++ eClinic/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(eClinic.Startup))]
+namespace eClinic
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
